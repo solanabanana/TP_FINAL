0f21652276df393623fac48b4015bb994910b9b6
Luiselys Medina <mluiselys@gmail.com>
2021-06-19 23:00:16 -0300

    agregando setters y getters de cerveza

--- TP_FINAL/cCerveza.h
+++ TP_FINAL/cCerveza.h
@@ -9,12 +9,31 @@ typedef enum {//enum de los tipos de cervezas que vende nuestra cervecería
 } eCerveza;
 class cCerveza
 {
-	int CantBarriles;//Barriles de 50L cada uno
-	const eCerveza Tipo;
-	
+	int CantBarriles;  //Barriles de 50L cada uno
+	float PrecioxLitro;
+	float Precio_Pinta;
+	float Precio_media_Pinta;
+	int CantLitros;
+	eCerveza Tipo;
+
 public:
-	cCerveza(eCerveza tipo, int cant_barriles) : Tipo(tipo) { CantBarriles = cant_barriles; };
+	cCerveza(eCerveza tipo, int cant_barriles, float PrecioxLitro, float Precio_Pinta, float Precio_media_Pinta) : Tipo(tipo)
+	{
+		CantBarriles = cant_barriles;
+		this->PrecioxLitro = PrecioxLitro;
+		this->Precio_Pinta = Precio_Pinta;
+		this->Precio_media_Pinta = Precio_media_Pinta;
+		CantLitros = 0;
+	};
 	~cCerveza() {};
-	//Necesitamos getter y setter de CantBarriles
-};
+
+	void setCant_Barriles(int num) { CantBarriles = num; }
+	void CalcLitros() { CantLitros = CantBarriles * 50; }
+	void setLitros(float Litros) { CantLitros = Litros; }
+	int getCantBarriles() { return CantBarriles; };
+	int getCantLitros() { return CantLitros; };
+	int getPrecioxLitro() { return PrecioxLitro; };
+	int getPrecio_Pinta() { return Precio_Pinta; };
+	int getprecio_medi_pinta() { return Precio_media_Pinta; };
+	eCerveza getTipo() { return Tipo; };
 
